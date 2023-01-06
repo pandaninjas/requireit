4891f1540f4a851f4acaecb3d538d51a6c8c4533
pandaninjas <admin@malwarefight.gq>
2023-01-06 00:51:17 +0000

    Code golf: Exception->G

--- requireitmini-inline.py
+++ requireitmini-inline.py
@@ -1,6 +1,6 @@
-from subprocess import check_output as A;import sys#git.io/JUVEE
-class VersionError(Exception):0
-class InstallError(Exception):0
+from subprocess import check_output as A;import sys;G=Exception#git.io/JUVEE
+class VersionError(G):0
+class InstallError(G):0
 E="Couldn't auto-install ";F=[sys.executable,"-m","pip","install"]
 def requireit(B):
 	for C in B:
@@ -10,4 +10,4 @@ def requireit(B):
 		try: globals()[J]=L(J)
 		except ModuleNotFoundError if sys.version_info.minor > 5 else ImportError:
 			try:A(F+[C]) if isinstance(C,str)else A(F+[C[1]]);globals()[J]=L(J)
-			except Exception:raise InstallError(E+J)
+			except G:raise InstallError(E+J)
